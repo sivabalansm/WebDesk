f132dfad36fa8f1db9d958310076a9f635e408b3
cfung89 <cyhfung@gmail.com>
2024-04-05 23:17:41 -0400

    rest fix

--- restAPI/test.py
+++ restAPI/test.py
@@ -4,16 +4,12 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-user_id = 1
+user_id = 0
 response = requests.put(BASE + "rest/" + str(user_id), {"name": "Cyrus", "machine_type": "Linux"})
 print(response.json())
 response = requests.get(BASE + "rest/" + str(user_id))
 print(response.json())
 response = requests.patch(BASE + "rest/" + str(user_id))
 print(response.json())
-"""
 response = requests.delete(BASE + "rest/" + str(user_id))
 print(response)
-response = requests.get(BASE + "rest/" + str(user_id))
-print(response)
-"""
